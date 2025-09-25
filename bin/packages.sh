15a39b140e52019d8ff855acc88edf2d768d933d
Giacomo Bagnoli <gbagnoli@gmail.com>
2025-09-25 11:32:28 +0200

    Install flatpak binaries and support wrappers to call cli tools

--- bin/packages.sh
+++ bin/packages.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 dotfiles="$(dirname "$script_dir")"
+bindir="${HOME}/.local/bin"
+
+create_wrapper() {
+  cat >"${bindir}/$1"
+  chmod +x "${bindir}/$1"
+}
 
 if command -v brew &>/dev/null; then
   echo >&2 "* Installing/upgrade brew bundle from ${dotfiles}/Brewfile"
@@ -30,4 +36,14 @@ if command -v uv &>/dev/null; then
    uv tool install git+https://github.com/gbagnoli/photo_process
 else
   echo >&2 "!! Skipping install of uv tools uv is not in path"
+fi
+
+
+if command -v flatpak &>/dev/null; then
+  xargs flatpak install --system -y < "$dotfiles/flatpak.txt"
+  # use 'EOL' to avoid variable expansion
+  cat <<'EOL' | create_wrapper keepassxc-cli
+#!/bin/bash
+exec flatpak run --command='keepassxc-cli' org.keepassxc.KeePassXC "$@"
+EOL
 fi
